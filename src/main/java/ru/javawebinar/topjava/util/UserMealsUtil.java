39854403b34dabfbf727a9ea15f12aba2d945c09
Пушкарев А. В <PushkarevAV@alfastrah.ru>
2019-02-09 17:51:20 +0700

    HW0. 1_2_HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -21,15 +21,11 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
         List<UserMealWithExceed> mealListWithExceed = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        for (UserMealWithExceed item : mealListWithExceed) {
-            System.out.println(item.toString());
-        }
+        mealListWithExceed.forEach(System.out::println);
 
         System.out.println("----------------------------");
         mealListWithExceed = getFilteredWithExceededStream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        for (UserMealWithExceed item : mealListWithExceed) {
-            System.out.println(item.toString());
-        }
+        mealListWithExceed.forEach(System.out::println);
     }
 
     private static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
